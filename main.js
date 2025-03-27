9e93fd91c0cce489848b43eae253283b251c0aa6
Gabriel <tauchen.gabriel@gmail.com>
2025-03-27 14:33:35 -0300

    Tipos Especiais

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function hello(person) {
-    return "Hello, ".concat(person);
+var promptSync = require("prompt-sync");
+var userPrompt = promptSync();
+// function hello(person: string): string{
+//     return `Hello, ${person}`
+// }
+// const userName = userPrompt("Please, enter your name: ")
+// console.log(hello(userName))
+//////////////////////////////////////////
+var qtdText = userPrompt("A quantidade de notas que o aluno tem: ");
+var qtd = Number(qtdText);
+var soma = 0;
+for (var i = 0; i < qtd; i++) {
+    var nTexto = userPrompt("Nota ".concat(i, ": "));
+    var n = Number(nTexto);
+    soma += n;
 }
-var userName = 'Gabriel';
-console.log(hello(userName));
+var media = soma / qtd;
+console.log("A m\u00E9dia do aluno \u00E9: ".concat(media));
--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
-function hello(person: string){
-    return `Hello, ${person}`;
-}
-let userName = 'Gabriel';
+const promptSync = require("prompt-sync")
+const userPrompt = promptSync()
 
-console.log(hello(userName));
+// function hello(person: string): string{
+//     return `Hello, ${person}`
+// }
+// const userName = userPrompt("Please, enter your name: ")
+
+// console.log(hello(userName))
+
+//////////////////////////////////////////
+
+let qtdText = userPrompt("A quantidade de notas que o aluno tem: ");
+let qtd: number = Number(qtdText);
+let soma: number = 0;
+for(let i=0;i<qtd;i++){
+    let nTexto = userPrompt(`Nota ${i}: `);
+    let n: number = Number(nTexto);
+    soma += n;
+}
+let media: number = soma/qtd;
+console.log(`A média do aluno é: ${media}`);
